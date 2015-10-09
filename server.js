2bef338ad5d56169106d010d37f78617809be991
Kaitlin Wiener <kaitlinwiener@Kaitlins-MacBook-Air.local>
2015-10-09 10:39:30 -0400

    added mongo_uri stuff

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require ('express'),
     PORT = process.env.PORT || 5432,
-    server = express();
+    server = express(),
+    MONGOURI = process.env.MONGOLAB_URI,
+    dbname = "some_userful_name",
+    mongoose.require('mongoose');
 
 
 server.get('/test', function (req, res) {
@@ -8,6 +11,8 @@ server.get('/test', function (req, res) {
   res.end();
 });
 
+
+mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function () {
   console.log("Server is up on port:", PORT);
 })
